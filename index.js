8e05c4df4d01f8d00b4adfe10ff5f986871c16b1
Christian99909 <cristian99909@gmail.com>
2026-05-27 19:00:51 -0600

    Implementa conexión a la base de datos y agrega nuevas rutas y vistas para 'viajes' y 'testimoniales'

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import router from './routes/index.js'
+import db from './config/db.js'
 
 const app = express();
+
+//conectar la base de datos
+db.authenticate()
+    .then(() => console.log('Base de datos conectada'))
+    .catch(error =>console.log(error));
 
 //Definir puerto
 const port = process.env.PORT || 4000;
@@ -15,7 +21,7 @@ app.use((req, res, next) => {
     const year = new Date();
 
     res.locals.actualYear = year.getFullYear();
-
+    res.locals.nombresitio = "Agencia de Viajes";
     next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,28 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { //req - lo que enviamos : res - lo que express nos responde
-    res.render('inicio');
+    
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
 });
 router.get('/nosotros', (req, res) => { //req - lo que enviamos : res - lo que express nos responde
    
-   const viajes = 'Cambiando el Texto';
-   
     res.render('nosotros',{
-        viajes
+        pagina: 'Nosotros'
     });
 });
+router.get('/viajes', (req, res) => { //req - lo que enviamos : res - lo que express nos responde
 
+    res.render('viajes',{
+        pagina: 'Viajes'
+    });
+});
+router.get('/testimoniales', (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+
+    res.render('testimoniales',{
+        pagina: 'Testimoniales'
+    });
+});
 
 export default router;
