82f69641e5f37d2617b5f8f5a93aad832a583f80
Christian99909 <cristian99909@gmail.com>
2026-05-28 21:44:53 -0600

    Implementa controladores para las páginas y define el modelo de 'Viaje'

---
+++ controllers/paginasController.js
@@ -0,0 +1,56 @@
+import {Viaje} from '../models/Viaje.js'
+
+
+const paginaInicio = (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
+}
+const paginaNosotros = (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+}
+const paginaViajes = async (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    
+    //Consultar base de datos
+
+    const viajes = await Viaje.findAll();
+    
+
+    res.render('viajes', {
+        pagina: 'Próximos Viajes',
+        viajes
+    });
+}
+const paginaTestimoniales = (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
+}
+
+//Muestra un viaje por su slug
+const paginaDetalleViaje = async (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    
+    const {slug} = req.params;
+
+    try {
+        const viaje = await Viaje.findOne({where : {slug}});
+
+        res.render('viaje',{
+            pagina: 'Informacion Viaje', 
+            viaje
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {
+
+    paginaInicio, paginaNosotros, paginaViajes, paginaTestimoniales, paginaDetalleViaje
+
+}
---
+++ models/Viaje.js
@@ -0,0 +1,14 @@
+import Sequalize from 'sequelize';
+import db from '../config/db.js'
+
+export const Viaje = db.define('viajes', {
+
+    titulo:{ type: Sequalize.STRING},
+    precio:{ type: Sequalize.STRING},
+    fecha_ida:{ type: Sequalize.DATE},
+    fecha_vuelta:{ type: Sequalize.DATE},
+    imagen:{ type: Sequalize.STRING},
+    descripcion:{ type: Sequalize.STRING},
+    disponibles:{ type: Sequalize.STRING},
+    slug:{ type: Sequalize.STRING},
+});
